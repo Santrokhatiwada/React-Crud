7afe33cf3e629aa3837ea392743a74b5d51759ff
SantroKhatiwada <santro.khatiwada98@gmail.com>
2023-10-18 14:00:37 +0545

    react

---
+++ src/components/Employees.js
@@ -0,0 +1,14 @@
+const Employees = [
+    
+    { id: "1", name: "Santro", age: "22" },
+
+
+    { id: "1", name: "Rupak", age: "23" },
+
+    { id: "1", name: "Sujan", age: "20" },
+
+    { id: "1", name: "Saurav", age: "19" },
+
+];
+
+export default Employees;
